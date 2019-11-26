8e0c665a3e57f3219731440819acc4014e217020
Denys M <dector9@gmail.com>
2019-11-26 06:54:48 +0400

    Checks for `hamming`.

---
+++ kotlin-assistant/src/main/kotlin/exercises/hamming/hamming.kt
@@ -0,0 +1,33 @@
+package exercises.hamming
+
+import assistant.Solution
+import assistant.requestSolution
+
+fun main() {
+    requestSolution("321e9dee9ebe4af1942945ca08146dd9") { solution ->
+        println("Few suggestions:")
+        with(solution) {
+            checkRequireIsUsed()
+            checkThrowsNotDeclared()
+            checkZipping()
+        }
+    }
+}
+
+private fun Solution.checkRequireIsUsed() {
+    if (text.contains("throw IllegalArgumentException")) {
+        println("- You can use [require()](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/require.html#require) function to verify arguments contract.")
+    }
+}
+
+private fun Solution.checkThrowsNotDeclared() {
+    if (text.contains("@Throws")) {
+        println("- There is no need to use `@Throws(Exception::class)` as Kotlin [doesn't have checked expections](https://kotlinlang.org/docs/reference/exceptions.html#checked-exceptions). So this annotation is mostly used if you are building library and you expect that this function will be called from Java code.")
+    }
+}
+
+private fun Solution.checkZipping() {
+    if (text.contains("for (") || text.contains("for(")) {
+        println("- If you want to go bit functionally - check-out [Zipping](https://kotlinlang.org/docs/reference/collection-transformations.html#zipping) and [`count {}`](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/count.html#count).")
+    }
+}
--- kotlin-assistant/src/main/kotlin/exercises/hamming/main.kt
+++
@@ -1,9 +0,0 @@
-package exercises.hamming
-
-import assistant.requestSolution
-
-fun main() {
-    requestSolution("321e9dee9ebe4af1942945ca08146dd9") { solution ->
-
-    }
-}
